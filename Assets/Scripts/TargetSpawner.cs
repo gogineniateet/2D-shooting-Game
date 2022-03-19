f569128094863adace4a297d4924a831f085a257
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-03-19 10:43:44 +0530

    game manager, target hit script.

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class TargetSpawner : MonoBehaviour
 {
     GameManager game;
-    public GameObject targetPrefab;   
-   
+    public GameObject targetPrefab;
+    //public float moveSpeed;
     void Start()
     {
         InvokeRepeating("Spawn", 1f, 1f);
         game = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    private void Update()
+    public void Update()
     {
+        //if (transform.position.x >= 0)
+        //    transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        //else if (transform.position.x <= 0)
+        //    transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
         if ((game.isGameWin == true) || (game.isGameOver == true))
         {
             CancelInvoke("Spawn");
@@ -23,10 +27,12 @@ public class TargetSpawner : MonoBehaviour
     public void Spawn()
     {
        if ((game.isGameWin == false) || (game.isGameOver == false))
-        {
+       {
             float randomX = UnityEngine.Random.Range(-4.5f, 4.5f);
             float randomY = UnityEngine.Random.Range(-2.3f, 2.3f);
-            Instantiate(targetPrefab, new Vector3(randomX, randomY, 0), Quaternion.identity);
-        }            
+            Instantiate(targetPrefab, new Vector3(randomX, randomY, 0), Quaternion.identity);            
+       }            
     }
+
+    
 }
